1995d19f800e7e5e4d0e31c884029e658f4cfe85
Abdul Moiz <moiz05366@gmail.com>
2025-06-12 03:51:10 +0500

    updated.

--- dist/index.js
+++ dist/index.js
@@ -7,3 +7,15 @@ str = 12;
 console.log(str);
 const bool = true;
 console.log(bool);
+const names = ["12", "42"];
+const user = {
+    name: "Abdul Moiz",
+    address: {},
+    id: 123,
+    email: "abc@gmail.com",
+    number: 12,
+};
+function abc(a, b) {
+    return a + b;
+}
+console.log(abc(12, 32));
--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,41 @@ const bool: boolean = true;
 console.log(bool);
 
 const names: string[] = ["12", "42"];
+
+
+type User = {
+    name: string,
+    address: object,
+    id: number,
+    email: string,
+    number: number
+}
+
+const user: User = {
+    name: "Abdul Moiz",
+    address: {},
+    id: 123,
+    email: "abc@gmail.com",
+    number: 12,
+};
+
+
+function abc(a: number, b: number): number {
+    return a + b;
+}
+
+const foo = (a: number, b: number): number => {
+    return a + b;
+}
+
+// optional parameter
+const opt = (a: string, b?: number) => {
+    console.log(a);
+}
+
+// Union Type in Typescript means the value can either a number or a string
+const num1: number | string = "12";
+
+console.log(abc(12, 32));
+
+

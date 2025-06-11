e1f0bf5b58522c62fee22d2716f02d3b082a3520
Abdul Moiz <moiz05366@gmail.com>
2025-06-12 02:32:17 +0500

    types & syntax

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+let num = 12;
+console.log(num);
+num = 14;
+let str = "Abdul Moiz";
+str = 12;
+console.log(str);
+const bool = true;
+console.log(bool);
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+let num: number = 12;
+console.log(num);
+
+
+num = 14;
+// num = "a" //we'll get an error. because I'm trying to assign string in number typed variable
+
+let str: any = "Abdul Moiz";
+str = 12;
+console.log(str);
+
+// In "any" type you can assign whatever u want. but It's not recommended way
+
+const bool: boolean = true;
+console.log(bool);
+
+const names: string[] = ["12", "42"];

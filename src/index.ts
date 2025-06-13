b521e79023a6043eed78df705c9dd0d39602a82f
Abdul Moiz <moiz05366@gmail.com>
2025-06-14 02:05:22 +0500

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+// number
+// string
+// Boolean
+// any
+// tuple
+// []
+// unKnown: unknow is similar to any.
+// void: when function doesn't return anything. so we can use this type
+// null and undefined.
+
 let num: number = 12;
 console.log(num);
 
@@ -38,9 +48,6 @@ function abc(a: number, b: number): number {
     return a + b;
 }
 
-const foo = (a: number, b: number): number => {
-    return a + b;
-}
 
 // optional parameter
 const opt = (a: string, b?: number) => {
@@ -51,5 +58,35 @@ const opt = (a: string, b?: number) => {
 const num1: number | string = "12";
 
 console.log(abc(12, 32));
+
+
+// this function will not return anything. so that's why I've used void here.
+const func = (): void => {
+    console.log("Hello world");
+}
+
+// there are two ways to define types
+// first by type keyword
+// second by interface
+
+// type Student = {
+//     name: string,
+//     course: string,
+//     phone: number,
+//     enroll: true,
+// };
+
+interface Student {
+    name: string,
+    course: string,
+    phone: number,
+    enroll: boolean,
+};
+const student: Student = {
+    name: "Moiz",
+    course: "Web and App Development",
+    phone: 1234567890,
+    enroll: true,
+};
 
 
